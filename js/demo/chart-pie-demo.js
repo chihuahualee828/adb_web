9d6c3021795a61fa934f0b646a77fe82003b390e
nicklee828 <46827904+nicklee828@users.noreply.github.com>
2023-01-18 01:57:53 +0800

    Add files via upload

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -2,34 +2,81 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false
-    },
-    cutoutPercentage: 80,
-  },
-});
+// Draw product pie chart (parameter = county/dist./season)
+function draw_pie_chart(searchText, searchhBy, parameter){
+	
+	
+	
+	console.log(searchText,searchhBy, parameter);
+	
+	$.ajax({
+		type: "POST",
+		url: 'php/chart_query.php',
+		dataType: 'text',
+		data: {	
+			searchText: searchText,
+			searchhBy: searchhBy,
+			parameter: parameter
+		},
+		success: function (response) {
+			console.log(response); //query results 2d array
+			response = JSON.parse(response);
+			response.shift();
+	
+			var labels = []
+			var data = []
+			var colors = [];
+			var hoverColors = [];
+			response.forEach(function(each) {
+				labels.push(each[0]);
+				data.push(each[1]);
+				colors.push(dynamicColors());
+				
+			});
+			
+			var ctx = document.getElementById("myPieChart");
+			ctx.width = 1000;
+			ctx.height = 1000;
+			var myPieChart = new Chart(ctx, {
+			  type: 'pie',
+			  data: {
+				labels: labels,
+				datasets: [{
+				  data: data,
+				  backgroundColor: colors,
+				  hoverBackgroundColor: colors,
+				  hoverBorderColor: "rgba(234, 236, 244, 1)",
+				}],
+			  },
+			  options: {
+				maintainAspectRatio: false,
+				tooltips: {
+				  backgroundColor: "rgb(255,255,255)",
+				  bodyFontColor: "#858796",
+				  borderColor: '#dddfeb',
+				  borderWidth: 1,
+				  xPadding: 10,
+				  yPadding: 10,
+				  displayColors: false,
+				  caretPadding: 10,
+				},
+				legend: {
+				  display: true
+				},
+				cutoutPercentage: 0,
+			  },
+			});
+		},
+		error: function(response) {
+			console.log(response);
+		}
+	});
+	
+}
+
+function dynamicColors(){
+	var r = Math.floor(Math.random() * 255);
+	var g = Math.floor(Math.random() * 255);
+	var b = Math.floor(Math.random() * 255);
+	return "rgb(" + r + "," + g + "," + b + ")";
+ };
